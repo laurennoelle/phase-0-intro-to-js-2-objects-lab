b0ddb6af0a9710299f119045801862ea3a11ac69
Lauren Erxleben <lauren.n.erxleben@gmail.com>
2022-06-23 18:26:47 -0600

    commit tree 0f4a94ae2255c3f7df2f0b79d78aa1091b3e7857

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your solution in this file!
+const employee = {
+    name : "Lauren Noelle",
+    streetAdress : "3608 Terry Lake Rd",
+};
+
+ function updateEmployeeWithKeyAndValue(employee, key, value) {
+    employee.name = "Sam"
+    return employee
+ };
